23ce7588d507d2caed35b6adb22446ff6616d827
jsomville <jsomville@hotmail.com>
2020-07-23 18:07:23 +0200

    build menu

---
+++ CelestialObjectLibrary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from ObjectLibrary import ObjectLibrary
+
+
+class CelestialObjectLibrary(ObjectLibrary):
+    celestialObjectList = list()
+
+    def __init__(self):
+        pass
